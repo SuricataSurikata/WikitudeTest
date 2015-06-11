b0794418dfe916487bee3fc676d74674298572a9
PatrycjaS <patrycja.sitek@besidethepark.com>
2015-06-11 12:33:34 +0200

    changed image in instruction message

--- WikitudeTest/ArchitectWorld/js/imageontarget.js
+++ WikitudeTest/ArchitectWorld/js/imageontarget.js
@@ -60,14 +60,14 @@ var World = {
 		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
 		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
 		document.getElementById('loadingMessage').innerHTML =
-			"<div" + cssDivLeft + ">Scan Target &#35;1 (surfer):</div>" +
-			"<div" + cssDivRight + "><img src='assets/surfer.png'></img></div>";
+			"<div" + cssDivLeft + ">Scan Target (lisa):</div>" +
+			"<div" + cssDivRight + "><img src='assets/lisasmall.png'></img></div>";
 
 		// Remove Scan target message after 10 sec.
 		setTimeout(function() {
 			var e = document.getElementById('loadingMessage');
 			e.parentElement.removeChild(e);
-		}, 10000);
+		}, 15000);
 	}
 };
 
